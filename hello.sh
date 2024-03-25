24852ed84fc515c9179ff2a5efbe7852d7171513
Satyanshu05 <145188925+Satyanshu05@users.noreply.github.com>
2024-03-25 20:39:51 +0530

    Update hello.sh
    Hello Team,
    There was an error I fixed it and updated.

--- hello.sh
+++ hello.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "Hello and Welcome to the session"
 echo "Good morning I'm from 2nd shift owner of the file"
+echo "Good morning I'm from 2nd shift owner of the file"
+echo "Good morning I'm from 2nd shift owner of the file"
+echo "Good morning I'm from 2nd shift owner of the file"
+echo "Good morning I'm from 2nd shift owner of the file"
+echo "Good morning I'm from 2nd shift owner of the file"
+echo "Good morning I'm from 2nd shift owner of the file"
